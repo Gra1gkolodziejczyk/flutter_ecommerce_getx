a9b63faffc18506b04b81a69dd0d1cd8cd4ab932
Jean Gravian <defreitas.flavio7@gmail.com>
2023-04-05 16:11:07 +0200

    Model

---
+++ lib/data/models/categories/categories_response_model.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'categories_response_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CategoriesResponseModel _$CategoriesResponseModelFromJson(
+        Map<String, dynamic> json) =>
+    CategoriesResponseModel(
+      id: json['id'] as String?,
+      name: json['name'] as String?,
+    );
+
+Map<String, dynamic> _$CategoriesResponseModelToJson(
+        CategoriesResponseModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+    };
---
+++ lib/data/models/user/user_response_model.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_response_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserRequestModel _$UserRequestModelFromJson(Map<String, dynamic> json) =>
+    UserRequestModel(
+      id: json['id'] as String?,
+      name: json['name'] as String?,
+      firstname: json['firstname'] as String?,
+      email: json['email'] as String?,
+      password: json['password'] as String?,
+      confirmpassword: json['confirmpassword'] as String?,
+    );
+
+Map<String, dynamic> _$UserRequestModelToJson(UserRequestModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'firstname': instance.firstname,
+      'email': instance.email,
+      'password': instance.password,
+      'confirmpassword': instance.confirmpassword,
+    };
