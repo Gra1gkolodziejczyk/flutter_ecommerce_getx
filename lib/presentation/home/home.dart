6fea73ad61c1195af878528f83b9899a87db2811
Gra1gkolodziejczyk <graig.kolodziejczyk@ynov.com>
2023-04-11 14:21:31 +0200

    all files

--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -1,4 +1,3 @@
-import 'package:e_commerce_front_getx/widgets/product_card.dart';
 import 'package:flutter/material.dart';
 import '../../core/app_export.dart';
 import '../../widgets/custom_appbar.dart';
@@ -16,18 +15,17 @@ class HomeScreen extends GetWidget<HomeScreenController> {
       body: SafeArea(
         child: Column(
           children: [
-            
             Obx(
               () => SizedBox(
                 height: MediaQuery.of(context).size.height - 149,
                 child: ListView.builder(
                   scrollDirection: Axis.vertical,
-                  itemCount: controller.categoriesList.length,
+                  itemCount: controller.categoriesWithProduct.length,
                   itemBuilder: (context, index) {
                     return Column(
                       children: [
                         Text(
-                          '${controller.categoriesList[index].name}',
+                          '${controller.categoriesWithProduct[index].name}',
                           style: const TextStyle(
                             fontSize: 25,
                             fontWeight: FontWeight.bold,
@@ -40,9 +38,53 @@ class HomeScreen extends GetWidget<HomeScreenController> {
                                 height: 500,
                                 child: ListView.builder(
                                   scrollDirection: Axis.horizontal,
-                                  itemCount: controller.productsList.length,
-                                  itemBuilder: (context, int index) {
-                                    return const ProductCard();
+                                  itemCount: controller
+                                      .categoriesWithProduct[index]
+                                      .products
+                                      ?.length,
+                                  itemBuilder: (context, index2) {
+                                    return Column(
+                                      children: [
+                                        Image.network(
+                                          '${controller.categoriesWithProduct[index].products![index2].image}',
+                                          width: 300,
+                                          height: 300,
+                                        ),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(
+                                              '${controller.categoriesWithProduct[index].products![index2].brand}',
+                                            ),
+                                            Text(
+                                              '${controller.categoriesWithProduct[index].products![index2].price}',
+                                            ),
+                                          ],
+                                        ),
+                                        Column(
+                                          children: [
+                                            Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment
+                                                      .spaceBetween,
+                                              children: [
+                                                ElevatedButton(
+                                                  child: const Text(
+                                                      "Voir détails"),
+                                                  onPressed: () {},
+                                                ),
+                                                ElevatedButton(
+                                                  child: const Text(
+                                                      "Ajouter au panier"),
+                                                  onPressed: () {},
+                                                ),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    );
                                   },
                                 ),
                               ),
