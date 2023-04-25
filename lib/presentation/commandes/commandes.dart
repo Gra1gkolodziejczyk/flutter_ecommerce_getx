b2ee5f25b3eff7c72c4dd00a7ef7572bb202d14f
Bastien Aumeunier <bastien.aumeunier@ynov.com>
2023-04-25 12:09:30 +0200

    add commandes.dart

--- lib/presentation/commandes/commandes.dart
+++ lib/presentation/commandes/commandes.dart
@@ -8,6 +8,11 @@ import 'package:intl/intl.dart';
 class CommandesScreen extends GetWidget<CommandesScreenController> {
   const CommandesScreen({Key? key}) : super(key: key);
 
+  final TextStyle text = const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      color: Color.fromARGB(255, 0, 0, 0));
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,32 +25,47 @@ class CommandesScreen extends GetWidget<CommandesScreenController> {
         child: FutureBuilder<int?>(
             future: controller.getCommandLenght(),
             builder: (BuildContext context, AsyncSnapshot<int?> snapshot) {
+              if (controller.getCommandLenght() == 0) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Commandes :', style: text),
+                    const Text("Vous n'avez pas encore de commandes"),
+                  ],
+                );
+              }
               if (snapshot.connectionState == ConnectionState.done) {
-                return ListView.builder(
-                  itemCount: snapshot.data ?? 0,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.zero,
-                        side: BorderSide(
-                          color: Colors.black,
-                          width: 1.0,
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                                "Prix: ${controller.commandList[index].price}€"),
-                            Text(
-                                "Faite le ${DateFormat('dd-MM-yyyy').format(controller.commandList[index].date)}")
-                          ],
-                        ),
-                      ),
-                    );
-                  },
+                return Column(
+                  children: [
+                    Text('Commandes :', style: text),
+                    Expanded(
+                        child: ListView.builder(
+                      itemCount: snapshot.data ?? 0,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                            side: BorderSide(
+                              color: Colors.black,
+                              width: 1.0,
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                    "Prix: ${controller.commandList[index].price}€"),
+                                Text(
+                                    "Faite le ${DateFormat('dd-MM-yyyy').format(controller.commandList[index].date)}")
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ))
+                  ],
                 );
               } else {
                 return const Center(
